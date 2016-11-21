f31835df0c591825f9d241a9bcbcd265fe98072a
zadime <c00201892@A310C004>
2016-11-21 16:02:16 +0000

    Started outputting

--- Game.cpp
+++ Game.cpp
@@ -8,11 +8,17 @@ Game::Game() : m_window(sf::VideoMode(900, 600, 32), "Controller", sf::Style::De
 }
 void Game::initialise()
 {
+	if (!font.loadFromFile("times.ttf"))
+	{
+	}
+
+
 	if (!m_background.loadFromFile("controller.jpg"))
 	{
 		std::cout << "Error Loading Background File" << std::endl;
 	}
 	m_sprite.setTexture(m_background);
+	initialiseText();
 	//m_sprite.setScale(0.1, 0.1);
 }
 
@@ -24,7 +30,7 @@ Game::~Game()
 void Game::run()
 {
 	initialise();
-	while (true)
+	while (m_window.isOpen())
 	{
 		update();
 		draw();
@@ -33,12 +39,96 @@ void Game::run()
 
 void Game::update()
 {
+	
 	gamePad.update();
+	leftAnalogX.setString(std::to_string((int)gamePad.m_currentState.LeftThumbStick.x));
+	rightAnalogX.setString(std::to_string((int)gamePad.m_currentState.RightThumbStick.y));
+	leftAnalogY.setString(std::to_string((int)gamePad.m_currentState.LeftThumbStick.y));
+	rightAnalogY.setString(std::to_string((int)gamePad.m_currentState.RightThumbStick.y));
+	LTrigger.setString(std::to_string((int)gamePad.m_currentState.LTrigger));
+	RTrigger.setString(std::to_string((int)gamePad.m_currentState.RTrigger));
 }
 
 void Game::draw()
 {
 	m_window.clear(sf::Color(1, 1, 1, 1));
+
 	m_window.draw(m_sprite);
+	
+	drawText();
 	m_window.display();
+}
+
+void Game::initialiseText()
+{
+	setTextProperties(pressedAText, 750,425);
+	setTextProperties(pressedBText,750,360);
+	setTextProperties(pressedXText,750,300);
+	setTextProperties(pressedYText,750,240);
+	setTextProperties(pressedStartText,720,425);
+	setTextProperties(leftAnalogX, 50, 265);
+	setTextProperties(leftAnalogY, 50, 285);
+	setTextProperties(leftAnalogPress, 720, 425);
+	setTextProperties(rightAnalogX, 720, 425);
+	setTextProperties(rightAnalogY, 720, 425);
+	setTextProperties(rightAnalogPressed, 720, 425);
+	setTextProperties(RBPressed, 720, 425);
+	setTextProperties(LBPressed, 720, 425);
+	setTextProperties(LTrigger, 720, 425);
+	setTextProperties(RTrigger, 720, 425);
+}
+void Game::setTextProperties(sf::Text &text, int x, int y)
+{
+	text.setFont(font);
+	text.setCharacterSize(24);
+	text.setColor(sf::Color::Red);
+	text.setString("Pressed");
+	text.setPosition(x, y);
+}
+void Game::drawText()
+{
+	if (gamePad.m_currentState.A)
+	{
+		m_window.draw(pressedAText);
+	}
+	if (gamePad.m_currentState.B)
+	{
+		m_window.draw(pressedBText);
+	}
+	if (gamePad.m_currentState.X)
+	{
+		m_window.draw(pressedXText);
+	}
+	if (gamePad.m_currentState.Y)
+	{
+		m_window.draw(pressedYText);
+	}
+	if (gamePad.m_currentState.back)
+	{
+		m_window.close();
+	}
+	if (gamePad.m_currentState.LeftThumbStick.x < -25 || gamePad.m_currentState.LeftThumbStick.x > 25)
+	{
+		m_window.draw(leftAnalogX);
+	}
+	if (gamePad.m_currentState.LeftThumbStick.y < -25 || gamePad.m_currentState.LeftThumbStick.y > 25)
+	{
+		m_window.draw(leftAnalogY);
+	}
+	if (gamePad.m_currentState.RightThumbStick.x < -25 || gamePad.m_currentState.RightThumbStick.x > 25)
+	{
+		m_window.draw(rightAnalogX);
+	}
+	if (gamePad.m_currentState.RightThumbStick.y < -25 || gamePad.m_currentState.RightThumbStick.y > 25)
+	{
+		m_window.draw(rightAnalogY);
+	}
+	if (gamePad.m_currentState.LTrigger < -10)
+	{
+		m_window.draw(LTrigger);
+	}
+	else if (gamePad.m_currentState.LTrigger > 10)
+	{
+		m_window.draw(RTrigger);
+	}
 }
--- Game.h
+++ Game.h
@@ -16,8 +16,32 @@ public:
 	void update();
 	void draw();
 private:
+	void initialiseText();
+	void setTextProperties(sf::Text &text, int x, int y);
+	void drawText();
 	sf::Texture m_background;
 	sf::Sprite m_sprite;
+
+
+
+
+
+	sf::Font font;
+	sf::Text pressedAText;
+	sf::Text pressedBText;
+	sf::Text pressedXText;
+	sf::Text pressedYText;
+	sf::Text pressedStartText;
+	sf::Text leftAnalogX;
+	sf::Text leftAnalogY;
+	sf::Text leftAnalogPress;
+	sf::Text rightAnalogX;
+	sf::Text rightAnalogY;
+	sf::Text rightAnalogPressed;
+	sf::Text RBPressed;
+	sf::Text LBPressed;
+	sf::Text LTrigger;
+	sf::Text RTrigger;
 	sf::RenderWindow m_window;
 	xbox360Controller gamePad;
 };
